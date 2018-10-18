b03df35843efd2ac326c53bfa49bce4a6bfddde1
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-10-18 23:31:58 +0300

    Add basic entities

---
+++ NutritionDiary.Entities/Food.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace NutritionDiary.Entities
+{
+    public class Food
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Measure> Measures { get; set; }
+    }
+}
