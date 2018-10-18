025aef4f0425e6e536f39c534737ec51fcb8f51a
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-10-18 23:33:30 +0300

    Add Db context

---
+++ NutritionDiary.Data/NutritionDiaryDb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NutritionDiary.Entities;
+
+namespace NutritionDiary.Data
+{
+    internal class NutritionDiaryDb : DbContext
+    {
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<Measure> Measures { get; set; }
+    }
+}
