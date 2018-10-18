7371cdf31179043aa1e8057a1b7d434714afccc7
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-10-19 00:10:09 +0300

    Update db context

--- NutritionDiary.Data/NutritionDiaryDb.cs
+++ NutritionDiary.Data/NutritionDiaryDb.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
 using NutritionDiary.Entities;
 
 namespace NutritionDiary.Data
@@ -14,7 +9,11 @@ namespace NutritionDiary.Data
         {
         }
 
+        public DbSet<ApiUser> ApiUsers { get; set; }
+        public DbSet<AuthToken> AuthTokens { get; set; }
         public DbSet<Food> Foods { get; set; }
         public DbSet<Measure> Measures { get; set; }
+        public DbSet<Diary> Diaries { get; set; }
+        public DbSet<DiaryEntry> DiaryEntries { get; set; }
     }
 }
