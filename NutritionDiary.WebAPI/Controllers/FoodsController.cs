1febd4e2d59475617085f4218be7b8ad1ad069e6
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-10-19 15:59:39 +0300

    Add foods controller

---
+++ NutritionDiary.WebAPI/Controllers/FoodsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NutritionDiary.Data;
+using NutritionDiary.Data.Interfaces;
+using NutritionDiary.Entities;
+
+namespace NutritionDiary.WebAPI.Controllers
+{
+    public class FoodsController : ApiController
+    {
+        private INutritionDiaryRepository _repository;
+
+        public FoodsController()
+        {
+            _repository = new NutritionDiaryRepository();
+        }
+
+        public FoodsController(INutritionDiaryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Food> Get()
+        {
+            return _repository.GetAllFoods()
+                              .OrderBy(f => f.Description)
+                              .Take(25)
+                              .ToList();
+        }
+    }
+}
