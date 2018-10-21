c09dc20abd172a5581c80b1ab5956d43ea2a027e
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-10-21 18:09:37 +0300

    Add models & model factory

---
+++ NutritionDiary.WebAPI/Models/MeasureModel.cs
@@ -0,0 +1,13 @@
+﻿namespace NutritionDiary.WebAPI.Models
+{
+    public class MeasureModel
+    {
+        public string Description { get; set; }
+        public int Calories { get; set; }
+        public double TotalFat { get; set; }
+        public double Protein { get; set; }
+        public double Carbohydrates { get; set; }
+        public double Fiber { get; set; }
+        public double Sugar { get; set; }
+    }
+}
---
+++ NutritionDiary.WebAPI/Models/ModelFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NutritionDiary.Entities;
+
+namespace NutritionDiary.WebAPI.Models
+{
+    public class ModelFactory
+    {
+        public FoodModel Create(Food food)
+        {
+            var model = new FoodModel
+            {
+                Description = food.Description,
+                Measures = food.Measures.Select(m => Create(m))
+            };
+
+            return model;
+        }
+
+        public MeasureModel Create(Measure measure)
+        {
+            var model = new MeasureModel
+            {
+                Description = measure.Description,
+                Calories = measure.Calories,
+                TotalFat = measure.TotalFat,
+                Protein = measure.Protein,
+                Carbohydrates = measure.Carbohydrates,
+                Sugar = measure.Sugar
+            };
+
+            return model;
+        }
+    }
+}
