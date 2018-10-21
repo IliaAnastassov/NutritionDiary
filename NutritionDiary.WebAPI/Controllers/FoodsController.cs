aaf5ea891e1b4d3698af9c29507bd34350233ddb
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-10-21 22:14:46 +0300

    Add url parameter to foods controller

--- NutritionDiary.WebAPI/Controllers/FoodsController.cs
+++ NutritionDiary.WebAPI/Controllers/FoodsController.cs
@@ -2,7 +2,6 @@
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Http;
-using NutritionDiary.Data;
 using NutritionDiary.Data.Interfaces;
 using NutritionDiary.Entities;
 using NutritionDiary.WebAPI.Models;
@@ -20,13 +19,23 @@ namespace NutritionDiary.WebAPI.Controllers
             _modelFactory = new ModelFactory();
         }
 
-        public IEnumerable<FoodModel> Get()
+        public IEnumerable<FoodModel> Get(bool includeMeasures = true)
         {
-            var foods = _repository.GetAllFoods()
-                                   .OrderBy(f => f.Description)
-                                   .Take(25)
-                                   .ToList()
-                                   .Select(f => _modelFactory.Create(f));
+            IQueryable<Food> query;
+
+            if (includeMeasures)
+            {
+                query = _repository.GetAllFoodsWithMeasures();
+            }
+            else
+            {
+                query = _repository.GetAllFoods();
+            }
+
+            var foods = query.OrderBy(f => f.Description)
+                             .Take(25)
+                             .ToList()
+                             .Select(f => _modelFactory.Create(f));
 
             return foods;
         }
