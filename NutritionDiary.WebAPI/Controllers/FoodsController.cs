b5a7cfaef20b7c90e908b439b7c24064b6c8c238
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-10-21 18:09:57 +0300

    Update foods controller

--- NutritionDiary.WebAPI/Controllers/FoodsController.cs
+++ NutritionDiary.WebAPI/Controllers/FoodsController.cs
@@ -5,26 +5,36 @@ using System.Web.Http;
 using NutritionDiary.Data;
 using NutritionDiary.Data.Interfaces;
 using NutritionDiary.Entities;
+using NutritionDiary.WebAPI.Models;
 
 namespace NutritionDiary.WebAPI.Controllers
 {
     public class FoodsController : ApiController
     {
         private INutritionDiaryRepository _repository;
+        private ModelFactory _modelFactory;
 
         public FoodsController(INutritionDiaryRepository repository)
         {
             _repository = repository;
+            _modelFactory = new ModelFactory();
         }
 
-        public IEnumerable<Food> Get()
+        public IEnumerable<FoodModel> Get()
         {
             var foods = _repository.GetAllFoods()
                                    .OrderBy(f => f.Description)
                                    .Take(25)
-                                   .ToList();
+                                   .ToList()
+                                   .Select(f => _modelFactory.Create(f));
 
             return foods;
+        }
+
+        public FoodModel Get(int id)
+        {
+            var food = _repository.GetFood(id);
+            return _modelFactory.Create(food);
         }
     }
 }
