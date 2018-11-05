b5299cc0270ad23a458c01bb368f1d61e0985145
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-11-05 22:58:14 +0200

    Add HTTPS required attribute

---
+++ NutritionDiary.WebAPI/Filters/RequireHttpsAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace NutritionDiary.WebAPI.Filters
+{
+    public class RequireHttpsAttribute : AuthorizationFilterAttribute
+    {
+        private const string html = "<div>Https required</div>";
+        private const int httpsPort = 443;
+
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            var request = actionContext.Request;
+
+            if (request.RequestUri.Scheme != Uri.UriSchemeHttps)
+            {
+                if (request.Method == HttpMethod.Get)
+                {
+                    actionContext.Response = request.CreateResponse(HttpStatusCode.Found);
+                    actionContext.Response.Content = new StringContent(html, Encoding.UTF8, "text/html");
+
+                    var uriBuilder = new UriBuilder(request.RequestUri);
+                    uriBuilder.Scheme = Uri.UriSchemeHttps;
+                    uriBuilder.Port = httpsPort;
+
+                    actionContext.Response.Headers.Location = uriBuilder.Uri;
+                }
+                else
+                {
+                    actionContext.Response = request.CreateResponse(HttpStatusCode.Forbidden);
+                    actionContext.Response.Content = new StringContent(html, Encoding.UTF8, "text/html");
+                }
+            }
+        }
+    }
+}
