aa40f7d56644cfa6fd23a8116b906dc02d7145d3
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-11-12 15:36:48 +0200

    Add custom unity filter provider

--- NutritionDiary.WebAPI/App_Start/UnityWebApiActivator.cs
+++ NutritionDiary.WebAPI/App_Start/UnityWebApiActivator.cs
@@ -1,5 +1,6 @@
 using System.Web.Http;
-
+using System.Web.Http.Filters;
+using NutritionDiary.WebAPI.Services;
 using Unity.AspNet.WebApi;
 
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NutritionDiary.WebAPI.UnityWebApiActivator), nameof(NutritionDiary.WebAPI.UnityWebApiActivator.Start))]
@@ -15,14 +16,18 @@ namespace NutritionDiary.WebAPI
         /// <summary>
         /// Integrates Unity when the application starts.
         /// </summary>
-        public static void Start() 
+        public static void Start()
         {
             // Use UnityHierarchicalDependencyResolver if you want to use
             // a new child container for each IHttpController resolution.
             // var resolver = new UnityHierarchicalDependencyResolver(UnityConfig.Container);
             var resolver = new UnityDependencyResolver(UnityConfig.Container);
-
             GlobalConfiguration.Configuration.DependencyResolver = resolver;
+
+            GlobalConfiguration.Configuration.Services.Add(
+                typeof(IFilterProvider),
+                new UnityFilterProvider(UnityConfig.Container)
+            );
         }
 
         /// <summary>
---
+++ NutritionDiary.WebAPI/Services/UnityFilterProvider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using Unity;
+
+namespace NutritionDiary.WebAPI.Services
+{
+    public class UnityFilterProvider : IFilterProvider
+    {
+        private IUnityContainer _container;
+
+        public UnityFilterProvider(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public IEnumerable<FilterInfo> GetFilters(HttpConfiguration configuration, HttpActionDescriptor actionDescriptor)
+        {
+            var controllerFilters = actionDescriptor.ControllerDescriptor
+                                                    .GetFilters()
+                                                    .Select(instance => new FilterInfo(instance, FilterScope.Controller));
+
+            var actionFilters = actionDescriptor.GetFilters()
+                                                .Select(instance => new FilterInfo(instance, FilterScope.Action));
+
+            var filters = controllerFilters.Concat(actionFilters);
+
+            foreach (var filter in filters)
+            {
+                _container.BuildUp(filter.Instance.GetType(), filter.Instance);
+            }
+
+            return filters;
+        }
+    }
+}
