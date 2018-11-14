96d331c69bb7b763df0d026a2d662d4aba303eed
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-11-14 10:46:56 +0200

    Refactor custom controller selector

--- NutritionDiary.WebAPI/Services/NutritionDiaryControllerSelector.cs
+++ NutritionDiary.WebAPI/Services/NutritionDiaryControllerSelector.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.Controllers;
@@ -11,6 +8,10 @@ namespace NutritionDiary.WebAPI.Services
 {
     public class NutritionDiaryControllerSelector : DefaultHttpControllerSelector
     {
+        private const string CONTROLLER_KEY = "controller";
+        private const string VERSIONED_CONTROLLER_FORMAT = "{0}V{1}";
+        private const string VERSION_QUERY_KEY = "v";
+        private const string DEFAULT_VERSION = "1";
         private HttpConfiguration _configuration;
 
         public NutritionDiaryControllerSelector(HttpConfiguration configuration)
@@ -23,12 +24,12 @@ namespace NutritionDiary.WebAPI.Services
         {
             var controllers = GetControllerMapping();
             var routeData = request.GetRouteData();
-            var controllerName = (string)routeData.Values["controller"];
+            var controllerName = (string)routeData.Values[CONTROLLER_KEY];
 
             if (controllers.TryGetValue(controllerName, out HttpControllerDescriptor descriptor))
             {
                 var version = GetVersionFrom(request);
-                var versionedControllerName = $"{controllerName}V{version}";
+                var versionedControllerName = string.Format(VERSIONED_CONTROLLER_FORMAT, controllerName, version);
 
                 if (controllers.TryGetValue(versionedControllerName, out HttpControllerDescriptor versionedControllerDescriptor))
                 {
@@ -42,11 +43,11 @@ namespace NutritionDiary.WebAPI.Services
         private string GetVersionFrom(HttpRequestMessage request)
         {
             var query = HttpUtility.ParseQueryString(request.RequestUri.Query);
-            var version = query["v"];
+            var version = query[VERSION_QUERY_KEY];
 
             if (string.IsNullOrWhiteSpace(version))
             {
-                version = "1";
+                version = DEFAULT_VERSION;
             }
 
             return version;
