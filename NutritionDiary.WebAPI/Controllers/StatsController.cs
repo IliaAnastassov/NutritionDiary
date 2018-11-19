400bae1e9b9af107ace7b7093236e9b8ff7fc921
Ilia Anastassov <ilia.anastassov@gmail.com>
2018-11-19 18:10:28 +0200

    Add stats controller

---
+++ NutritionDiary.WebAPI/Controllers/StatsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NutritionDiary.Data.Interfaces;
+
+namespace NutritionDiary.WebAPI.Controllers
+{
+    public class StatsController : BaseApiController
+    {
+        public StatsController(INutritionDiaryRepository repository)
+            : base(repository)
+        {
+        }
+
+        [Route("api/stats")]
+        public IHttpActionResult Get()
+        {
+            var model = new
+            {
+                FoodCount = Repository.GetAllFoods().Count()
+            };
+
+            return Ok(model);
+        }
+    }
+}
